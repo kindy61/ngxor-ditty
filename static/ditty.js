b6b2025daa3384affdd405bdf91651a396385615
kindy lin <kindy61@gmail.com>
2010-10-10 13:01:24 +0800

    init code

---
+++ static/ditty.js
@@ -0,0 +1,150 @@
+// base on http://github.com/namelessjon/ditty
+
+jQuery(function($) {
+  // let tiddlers be closed
+  $("div.ditty a[rel='close']").live('click',
+    function() {
+      $(this).parents('.ditty').slideUp('fast', function() { $(this).remove(); } );
+      return false;
+    }
+  );
+
+  // make get links work
+  $("a.existing").live('click',
+    function() {
+      var tag = this;
+      var ditty_name = $(this).attr('title');
+      var ditty = $("div.ditty#" + ditty_name);
+      if (ditty.size() == 0) {
+      $.get(this, function(data, status) {
+          $(tag).parents('.ditty').after(data);
+          }); 
+      };
+      return false;
+    }
+  );
+
+  // make the editing form cancellable
+  $("div.ditty_edit ul.edit_links a[rel='cancel']").live('click',
+    function() {
+      $(this).parents('.ditty_edit').prev().slideDown('fast');
+      $(this).parents('.ditty_edit').remove();
+      return false;
+    }
+  );
+
+  // make the new link work.
+  $("a[href='/new']").live('click',
+    function() {
+      var tag = this
+      $.get(this, function(data, status) {
+        $('#ditties').prepend(data);
+      });
+      return false;
+    }
+  );
+
+  // make a new link work.
+  $("a.new_ditty").live('click',
+    function() {
+      var tag = this
+      $.get(this, function(data, status) {
+        $(tag).parents('.ditty').prepend(data);
+      });
+      return false;
+    }
+  );
+
+  // make edit links work
+  $("div.ditty a[rel='edit']").live('click',
+    function() {
+      var tag = this;
+      $.get(this, function(data, status) {
+        $(tag).parents('.ditty').after(data);
+        $(tag).parents('.ditty').hide();
+      });
+      return false;
+    }
+  );
+
+  // make delete links work
+  $("div.ditty_edit a[rel='destroy']").live('click',
+    function() {
+      var tag = this;
+      $.ajax({
+        url: this,
+        type: 'POST',
+        timeout: 5000,
+        data: {_method: 'DELETE' },
+        success: function (data, status) {
+          // we return the title, so!
+          $("#" + data).remove();
+          $("#edit_" + data ).remove();
+        },
+        error: function (xhr, status) {
+          alert(xhr.responseText);
+        }
+      });
+      return false;
+    }
+  );
+
+  // make done links work
+  $("div.ditty_edit a[rel='update']").live('click',
+    function() {
+      var tag = this;
+      var form_data = {};
+      form_data._method = 'PUT';
+
+      // find the form
+      var form = $(tag).parents('.ditty_edit').children('form');
+      form_data.title = $(form).children("input[name='ditty_title']").val();
+      form_data.body = $(form).children("textarea[name='ditty_body']").val();
+
+      $.ajax({
+        url: this,
+        type: 'POST',
+        timeout: 5000,
+        data: form_data,
+        success: function (data, status) {
+          $(tag).parents('.ditty_edit').prev().replaceWith(data);
+          $(tag).parents('.ditty_edit').remove();
+        },
+        error: function (xhr, status) {
+          alert(xhr.responseText);
+        }
+      });
+      return false;
+    }
+  );
+
+  // make done links work on create forms
+  $("div.ditty_edit a[rel='create']").live('click',
+    function() {
+      var tag = this;
+      var form_data = {};
+
+      // find the form
+      var form = $(tag).parents('.ditty_edit').children('form');
+      form_data.title = $(form).children("input[name='ditty_title']").val();
+      form_data.body = $(form).children("textarea[name='ditty_body']").val();
+
+      $.ajax({
+        url: this,
+        type: 'POST',
+        timeout: 5000,
+        data: form_data,
+        success: function (data, status) {
+          $(tag).parents('.ditty_edit').after(data);
+          $(tag).parents('.ditty_edit').remove();
+          $("a[title='" + form_data.title + "'].new_ditty").addClass('existing').removeClass('new_ditty').attr('href', "/"+ form_data.title);
+        },
+        error: function (xhr, status) {
+          alert(xhr.responseText);
+        }
+      });
+      return false;
+    }
+  );
+
+}); // close ready block
---
+++ static/tmpl.js
@@ -0,0 +1,66 @@
+(function($){
+  var cache = {};
+
+  function compl(str) {
+    return "var p=[],print=function(){p.push.apply(p,arguments);};" +
+
+    // Introduce the data as local variables using with(){}
+    "with(arguments[0]){p.push('" +
+
+    // Convert the template into pure JavaScript
+    str
+      .replace(/[\r\t\n]/g, " ")
+      .split("<%").join("\t")
+      .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+      .replace(/\t=(.*?)%>/g, "',$1,'")
+      .split("\t").join("');")
+      .split("%>").join("p.push('")
+      .split("\r").join("\\'")
+
+    + "');}return p.join('');";
+  }
+
+  function tmpl(str, data){
+    // Figure out if we're getting a template, or if we need to
+    // load the template - and be sure to cache the result.
+    var name;
+    if (str instanceof Array) {
+      name = str[0];
+      str = str[1];
+    }
+
+    try {
+      var fn = new Function(compl(str));
+      name && (cache[name] = fn);
+    } catch (ex) {
+      return null;
+    }
+
+    // Provide some basic currying to the user
+    return data ? fn( data ) : fn;
+  }
+
+  $.tmpl = tmpl;
+  tmpl.get = function (name) {
+    return cache[name] || null;
+  };
+  tmpl.clear = function () {
+    cache = {};
+  }
+})(window);
+
+/*
+<div id="<%=id%>" class="<%=(i % 2 == 1 ? " even" : "")%>">
+  <div class="grid_1 alpha right">
+    <img class="righted" src="<%=profile_image_url%>"/>
+  </div>
+  <div class="grid_6 omega contents">
+    <p><b><a href="/<%=from_user%>"><%=from_user%></a>:</b> <%=text%></p>
+  </div>
+</div>
+
+<% for ( var i = 0; i < users.length; i++ ) { %>
+  <li><a href="<%=users[i].url%>"><%=users[i].name%></a></li>
+<% } %>
+
+*/
